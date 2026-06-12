7137d9bff68dff041e2e763e4e2bb3ddba852dc0
rimahatib51-star <rimahatib51@gmail.com>
2026-06-12 20:49:45 +0300

    Add showMessage function to display project info

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage(){
+
+    document.getElementById("message").innerHTML =
+    "Bu proje PyQt5 grafik arayüzü ile çalışan bir Python oyunudur. Çalıştırmak için sports_card_game.py dosyasını indirip Python ile çalıştırmalısınız.";
+
+}
